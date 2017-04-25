49ee30c3a6dd27f5bf4e79d54391d33be2a616ba
Sarah Sexton <sarahjeannesexton@live.com>
2017-04-25 15:51:12 -0500

    Updating index.html

--- public/JavaScript.js
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript">
-jQuery(".parent_div .titlebar_div").click(function(){$(".parent_div .chat_div").slideToggle();});
-jQuery(".parent_div .chat_div").hide();
-</script>
