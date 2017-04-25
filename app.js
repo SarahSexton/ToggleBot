624791d0ed77f375092363178f28fc781a5c6274
Sarah Sexton <sarahjeannesexton@live.com>
2017-04-25 15:36:41 -0500

    Pointing chat start page to index.html

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var builder = require('botbuilder')
 var cognitiveservices = require('botbuilder-cognitiveservices')
 
 var botConnectorOptions = {
-  appId: process.env.APP_ID,
-  appPassword: process.env.APP_PASS
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_PASS
 }
 
 // Create bot
@@ -16,18 +16,27 @@ var bot = new builder.UniversalBot(connector)
 // Set up restify server
 var server = restify.createServer()
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('listening to: ', server.name, server.url)
+    console.log('listening to: ', server.name, server.url)
 })
 server.post('/api/messages', connector.listen())
 
+// Serve the embded HTML chat bot on our index.html page. Users will interact w/ the bot there.
+// Look in the public folder and grab the index.html page
+server.get(/\/?.*/, restify.serveStatic({
+    directory: './public',
+    default: 'index.html'
+}));
+
 // Luis Setup
 var qnaRecognizer = new cognitiveservices.QnAMakerRecognizer({
-  knowledgeBaseId: process.env.KNOWLEDGE_BASE,
-  subscriptionKey: process.env.SUBSCRIPTION_KEY})
+    knowledgeBaseId: process.env.KNOWLEDGE_BASE,
+    subscriptionKey: process.env.SUBSCRIPTION_KEY
+})
 
 var BasicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
-  recognizers: [qnaRecognizer],
-  defaultMessage: 'No good match in the FAQ. Try changing your query terms.',
-  qnaThreshold: 0.5})
+    recognizers: [qnaRecognizer],
+    defaultMessage: 'No good match in the FAQ. Try changing your query terms.',
+    qnaThreshold: 0.5
+})
 
 bot.dialog('/', BasicQnAMakerDialog)
